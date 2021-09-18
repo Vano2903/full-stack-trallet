c9b97de878338ae6a7d9424f1baf0cc93b3a3221
francescodandrea <francescodandrea339@gmail.com>
2021-09-18 02:21:30 +0200

    Login page updates till break

--- front-end/User/DATAscript.js
+++ front-end/User/DATAscript.js
@@ -1,1 +1,32 @@
-//placeholder
+
+
+function login() {
+    let ok=1;
+
+    let password=document.querySelector("#floatingPassword");
+    let email=document.querySelector("#floatingEmail");
+
+    if(document.querySelector("#floatingEmail").value==""){
+        document.querySelector("#floatingEmail").value="";
+        document.querySelector("#floatingEmail").classList.add("is-invalid");
+        document.querySelector("#emailF").style.display="inherit";
+        ok=0;
+    }
+    if(document.querySelector("#floatingEmail").value.includes){
+        document.querySelector("#floatingEmail").value="";
+        document.querySelector("#floatingEmail").classList.add("is-invalid");
+        document.querySelector("#emailF").style.display="inherit";
+        ok=0;
+    }
+    if(document.querySelector("#floatingPassword").value==""){
+        document.querySelector("#floatingPassword").value="";
+        document.querySelector("#floatingPassword").classList.add("is-invalid");
+        document.querySelector("#passF").style.display="inherit";
+        ok=0;
+    }
+
+    if(ok){
+        console.log("Richiesta inviata al server");
+        document.querySelector("#login > form > button").disabled=true;
+    }
+}
