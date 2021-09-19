f406de658fb59669ef3eaa4ddf37495ba877733d
francescodandrea <francescodandrea339@gmail.com>
2021-09-19 11:44:11 +0200

    Gate totem and manager setup

--- static/Gates/GateManager/UIscript.js
+++ static/Gates/GateManager/UIscript.js
@@ -1,1 +1,22 @@
-//placeholder
+/*######## BASE #########*/
+
+/*######## add document #########*/
+function docrefresh(){
+    //a
+}
+
+function adddoctoggle(){
+    document.querySelector("#adddoc").classList.toggle("open");
+    if(document.querySelector("#adddoc").classList.contains("open"))
+        document.querySelector("#adddoc>h1").innerHTML="-";
+    else
+        document.querySelector("#adddoc>h1").innerHTML="+";
+}
+function adddoc(type){
+    //type can be: id, pasp, ticket, visa, green, swab
+    console.log("aggiungi "+type);
+}
+
+function updateRequirements(div,type){
+    div.classList.toggle("requested");
+}
